ade571aa37abbec329e6554ede94bd8028ec740d
Jeongin Lee <68997923+jjeongin@users.noreply.github.com>
2021-12-14 15:53:14 +0400

    Add files via upload

---
+++ jeong-in-lee/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    var index;
+    var text_index;
+    $(".work-right").hide();
+    
+    $(".work").hover(function(){
+        index = $(this).index();
+        $(".work-right").hide(); // hide all the other descriptions
+        $(".work-right").eq(index).show(); // only display this description
+    },);
+
+    $(".name-description-text").hover(function(){
+        // text_index = $(this).index();
+        $('.name-description-text').css("font-weight", 'normal');
+        $(this).css("font-weight", 'bold');
+        // $('p').css("font-family", 'Helvetica');
+        // $('p').eq(text_index).css("font-family", 'Lora');
+    })
+});
